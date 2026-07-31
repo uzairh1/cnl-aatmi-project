b15c6dfecfd465f9e13297862f9a0d87fbd417c2
Uzair Hammad <uzairhammad@hotmail.com>
2026-07-31 11:57:15 -0600

    all unit tests pass!

--- unit_tests/conftest.py
+++ unit_tests/conftest.py
@@ -1,6 +1,12 @@
 from pathlib import Path
 import sys
 
+import os
+os.environ["MPLBACKEND"] = "Agg"
+
+import matplotlib
+matplotlib.use("Agg")
+
 ROOT = Path(__file__).resolve().parents[1]
 if str(ROOT) not in sys.path:
     sys.path.insert(0, str(ROOT))
